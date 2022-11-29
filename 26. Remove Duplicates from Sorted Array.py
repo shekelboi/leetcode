95495a606fcdba09e7e31580d1126ebc708bcf29
Kevin <kevin005005@gmail.com>
2022-11-29 01:44:43 +0100

    26. Remove Duplicates from Sorted Array - much more elegant solution with much better time complexity

--- 26. Remove Duplicates from Sorted Array.py
+++ 26. Remove Duplicates from Sorted Array.py
@@ -1,17 +1,18 @@
 def removeDuplicates(nums):
-    number_of_duplicates = 0
-    counter = 0
-    while counter < len(nums) - 1 and nums[counter] != "_":
-        if nums[counter] == nums[counter + 1]:
-            number_of_duplicates += 1
-            nums[counter + 1] = "_"
-            for j in range(counter + 2, len(nums)):
-                temp = nums[j - 1]
-                nums[j - 1] = nums[j]
-                nums[j] = temp
-        else:
-            counter += 1
-    return len(nums) - number_of_duplicates
+    unique = 1
+    left = 0
+    right = left + 1
+    while right < len(nums):
+        if nums[left] != nums[right]:
+            unique += 1
+            left += 1
+            nums[left] = nums[right]
+        right += 1
+
+    for i in range(unique, len(nums)):
+        nums[i] = "_"
+
+    return unique
 
 
 nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
