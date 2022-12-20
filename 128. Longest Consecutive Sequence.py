7162982e2981cd8e4ec2a778f9db45f32eaee066
Kevin <kevin005005@gmail.com>
2022-12-20 17:11:58 +0100

    128. Longest Consecutive Sequence - worked in some cases lol

---
+++ 128. Longest Consecutive Sequence.py
@@ -0,0 +1,26 @@
+def longestConsecutive(nums):
+    if len(nums) == 0:
+        return 0
+    sequence_dict = {}
+    for n in nums:
+        if n not in sequence_dict:
+            sequence_dict[n] = 1
+            if n + 1 in sequence_dict:
+                sequence_dict[n] += sequence_dict[n + 1]
+                del sequence_dict[n + 1]
+            if n - 1 in sequence_dict:
+                sequence_dict[n - 1] = sequence_dict[n] + 1
+                del sequence_dict[n]
+
+    for k, v in sequence_dict.items():
+        while v is not None and k + v in sequence_dict and sequence_dict[k + v] is not None:
+            sequence_dict[k] += sequence_dict[k + v]
+            sequence_dict[k + v] = None
+            v = sequence_dict[k]
+
+    print(sequence_dict)
+    return max([v for v in sequence_dict.values() if v is not None])
+
+
+nums = [1, 2, 0, 1]
+print(longestConsecutive(nums))
