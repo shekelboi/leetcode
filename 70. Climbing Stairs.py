7c6470d007a31d7e303fdf74a13dfb6a4d305582
Kevin <kevin005005@gmail.com>
2023-01-02 00:02:19 +0100

    70. Climbing Stairs - just Fibonacci

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -1,11 +1,12 @@
 def climbStairs(n):
-    results = [[1 for i in range(n)]]
-    max_number_of_twos = n // 2
-    for i in range(1, max_number_of_twos + 1):
-        arr = [2 for j in range(i)]
-        arr.extend([1 for j in range(n - i * 2)])
-        results.append(arr)
-    return results
+    previous = 1
+    current = 1
+    for i in range(n - 1):
+        print(current)
+        temp = current
+        current = current + previous
+        previous = temp
+    return current
 
 
-print(climbStairs(10))
+print(climbStairs(4))
