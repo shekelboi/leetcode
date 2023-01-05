e67f6dcba65ea798a925e3c6a45e1fae325e1b17
Kevin <kevin005005@gmail.com>
2023-01-05 14:31:07 +0100

    2441. Largest Positive Integer That Exists With Its Negative - optimized solution

--- 2441. Largest Positive Integer That Exists With Its Negative.py
+++ 2441. Largest Positive Integer That Exists With Its Negative.py
@@ -7,5 +7,21 @@ def find_max_k_naive(nums):
     return found
 
 
+def find_max_k_optimized(nums):
+    nums_set = set()
+    max_num = -1
+    for n in nums:
+        inverse = -1 * n
+        if inverse not in nums_set:
+            nums_set.add(n)
+        else:
+            val = abs(n)
+            if val > max_num:
+                max_num = val
+            nums_set.remove(inverse)
+
+    return max_num
+
+
 nums = [-1, 2, -3, 3]
-print(find_max_k_naive(nums))
+print(find_max_k_optimized(nums))
