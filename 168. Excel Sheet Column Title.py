e5235c4f294be7a3a34c49722ea28edbdbdc390b
Kevin <kevin005005@gmail.com>
2023-06-12 20:17:12 +0200

    171. Excel Sheet Column Number - definitely much easier

--- 168. Excel Sheet Column Title.py
+++ 168. Excel Sheet Column Title.py
@@ -1,15 +1,3 @@
-# Exact opposite of what needs to be done
-# def convertToTitle(columnNumber: int) -> str:
-#     num_value = ord('A') - 1
-#     res = 0
-#     for i, v in enumerate(columnNumber):
-#         base = 26 ** i
-#         res += base * v
-#     return res
-#
-#
-# columnNumber = 28
-
 def convertToTitle(columnNumber: int) -> str:
     num_value = ord('A')
     output = ''
